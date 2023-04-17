cbffaad812de8e4fabaa6d714fdeda526b585211
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-04-17 08:11:49 +0800

    Add questionnaire data

--- Pages/Bangkok/BangkokViewData.swift
+++ Pages/Bangkok/BangkokViewData.swift
@@ -21,7 +21,7 @@ import Foundation
 struct TempleData {
     var name: String
     var description: String
-    var questions: [MCQuestion]
+    var question: MCQuestion
 
     static let temples: [TempleData] = [
         .init(name: "Sanctuary of Truth",
@@ -33,16 +33,20 @@ Buddhist and Hindu beliefs.
 Notably, the building is constructed entirely out of wood, with wood-carved idols and sculptures. \
 The temple houses an internal space of 2,115 m2, with the tallest spire reaching to a staggering 30 metres (98 feet).
 """,
-              questions: [
-                .init(question: "Something",
-                      options: [
-                        "option 1",
-                        "option 2",
-                        "option 3",
-                        "option 4"
-                      ],
-                      correctOption: 1)
-              ]),
+              question: .init(question: "What is the inspiration behind the Sanctuary of Truth's architecture?",
+                              options: [
+                                "Greek and Roman Mythology",
+                                "Buddhist and Hindu Traditions",
+                                "Mayan Culture",
+                                "Chinese Architecture"
+                              ],
+                              correctOption: 1,
+                              explanation: """
+The Sanctuary of Truth's architecture is heavily influenced by Buddhist and Hindu traditions. \
+The temple features intricate carvings and sculptures that depict scenes from Buddhist and Hindu \
+mythology. The temple's design is meant to represent the ancient philosophy of Earth, Wind, Fire, and Water.
+""")
+              ),
         .init(name: "Wat Pa Maha Chedi Kaew (Temple of a Million Bottles)",
               description: """
 Wat Pa Maha Chedi Kaew, also known as the Temple of a Million \
@@ -53,16 +57,22 @@ some 20 buildings had been similarly constructed.
 The China Daily has said that "The Thai Buddhist temple has found an environmentally \
 friendly way to utilize discarded bottles to reach nirvana (a form of buddhist inner peace)"
 """,
-              questions: [
-                .init(question: "Something",
-                      options: [
-                        "option 1",
-                        "option 2",
-                        "option 3",
-                        "option 4"
-                      ],
-                      correctOption: 1)
-              ]),
+              question: .init(question: "Who was the primary driving force behind the construction of the Temple of a Million Bottles?",
+                              options: [
+                                "A Buddhist monk",
+                                "A local artist",
+                                "A group of environmental activists",
+                                "A government official"
+                              ],
+                              correctOption: 0,
+                              explanation: """
+The Temple of a Million Bottles was the brainchild of a Buddhist monk named Pra Khru Vijai, \
+who saw the abundance of discarded glass bottles in his community as an opportunity to create \
+something unique and environmentally friendly. With the help of his followers and local volunteers, \
+he collected over a million bottles and used them to build the temple. The temple has since become \
+a popular attraction and a symbol of sustainable architecture.
+""")
+              ),
         .init(name: "Wat Ban Kung (Temple in a tree)",
               description: """
 Wat Bang Kung (Thai: วัดบางกุ้ง) is an ancient temple in Samut Songkhram, Thailand. It was \
@@ -75,32 +85,42 @@ commonly called Luang Phot Bot Noi. Inside the hall, there is a mural that shows
 Buddha. The Fine Arts Department has registered Wat Bang Kung as a national archaeological \
 site on December 18, 1996.
 """,
-              questions: [
-                .init(question: "Something",
-                      options: [
-                        "option 1",
-                        "option 2",
-                        "option 3",
-                        "option 4"
-                      ],
-                      correctOption: 1)
-              ]),
+              question: .init(question: "What is the most striking feature of Wat Ban Kung?", // TODO: Verify this
+                              options: [
+                                "The Buddha Image",
+                                "The Garden",
+                                "The Wood Carvings",
+                                "The Traditional Motifs"
+                              ],
+                              correctOption: 0,
+                              explanation: """
+Explanation: The most prominent feature of Wat Ban Kung is its ordination hall, which houses a \
+large and highly revered Buddha image. The hall is made of wood and decorated with intricate \
+traditional motifs, but the Buddha image is the main attraction for visitors and pilgrims.
+""")
+              ),
         .init(name: "Wat Chaiwatthanaram (Temple of Long Reign and Glorious Era)",
               description: """
-Located in Ayutthaya, the old capital of Thailand, Wat Chaiwatthanaram (Thai: วัดไชยวัฒนาราม) is a \
+Located in Ayutthaya, the old capital of Siam (old Thailand), Wat Chaiwatthanaram (Thai: วัดไชยวัฒนาราม) is a \
 Buddhist temple on the west bank of the Chao Phraya River, outside Ayutthaya island. It is one of \
 Ayutthaya's best known temples and a major tourist attraction.
 """,
-              questions: [
-                .init(question: "Something",
-                      options: [
-                        "option 1",
-                        "option 2",
-                        "option 3",
-                        "option 4"
-                      ],
-                      correctOption: 1)
-              ]),
+              question: .init(question: "What is the significance of the number of smaller prangs surrounding the central prang at Wat Chaiwatthanaram?",
+                              options: [
+                                "It represents the 12 animals of the Thai zodiac",
+                                "It symbolizes the 8-fold path of Buddhism",
+                                "It corresponds to the 16 provinces of Ayutthaya",
+                                "It mirrors the 9 levels of the Buddhist heaven"
+                              ],
+                              correctOption: 1,
+                              explanation: """
+The number of smaller prangs surrounding the central prang at Wat Chaiwatthanaram corresponds \
+to the 9 levels of the Buddhist heaven. The temple's design is meant to represent Mount Meru, \
+which is considered to be the center of the universe in Buddhist cosmology. The prangs, chedis, \
+and viharas surrounding the central prang are meant to represent the mountains and continents \
+that surround Mount Meru in Buddhist mythology.
+""")
+              ),
         .init(name: "White Temple",
               description: """
 The temple was constructed in 1630 by the king, Prasat Thong, as the first temple of his reign, \
@@ -109,16 +129,20 @@ of long reign and glorious era. It was designed in Khmer style to gain Buddhist 
 memorial to his mother, however Prince Damrong believed it was built to celebrate Ayutthaya \
 Kingdom's victory over Longvek.
 """,
-              questions: [
-                .init(question: "Something",
-                      options: [
-                        "option 1",
-                        "option 2",
-                        "option 3",
-                        "option 4"
-                      ],
-                      correctOption: 1)
-              ])
+              question: .init(question: "What does the bridge leading up to the temple represent?",
+                              options: [
+                                "Attaining enlightment",
+                                "The way to happyness",
+                                "The path to the afterlife",
+                                "Raising oneself above others"
+                              ],
+                              correctOption: 1,
+                              explanation: """
+The main building at the white temple, the ubosot, is reached by crossing a bridge over a small lake. \
+In front of the bridge are hundreds of outreaching hands that symbolize unrestrained desire. The bridge \
+proclaims that the way to happiness is by foregoing temptation, greed, and desire.
+""")
+              )
     ]
 }
 
@@ -126,4 +150,5 @@ struct MCQuestion {
     var question: String
     var options: [String]
     var correctOption: Int
+    var explanation: String
 }
